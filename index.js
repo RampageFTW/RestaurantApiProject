c1b9bc014b83cabc0a374064e11394b24dae65c4
Carlos Segoviano <halokid626@hotmail.com>
2019-04-20 15:06:35 -0700

    added picture in results and added color styles

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function renderResult(result, query) {
 	const text_noimage = `
 	<div class="row">
 	<div class="col-2">
-	<img src="http://stevenbeyerjr.com/img/rest.jpg" class="thumb-img" alt="restaurant">
+	<img src="rest.jpg" class="thumb-img" alt="restaurant">
 	</div>
 	<div class="col-6">
 	<h2 class="restaurant-name">${rest.restaurant.name}</h2><br>
