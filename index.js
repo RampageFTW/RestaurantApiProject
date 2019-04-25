23381acb95698aa5aa5c7d072c0d6a3f1622f1e9
Carlos Segoviano <halokid626@hotmail.com>
2019-04-24 23:55:41 -0700

    fixed js and added responsiveness. also added minor css font styles

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 
 //step 2: with the input from the user make the api call.
 //this is how to get restaurant location data
-function getLatLongByCityName(cityName, query) {
-    const searchURL = `https://developers.zomato.com/api/v2.1/search?q=${cityName}`;
+
+function getCityIdByCityName(cityName, query) {
+  const searchURL = `https://developers.zomato.com/api/v2.1/cities?q=${cityName}`;
+  $.ajax({
+      type: 'GET',
+      url: searchURL,
+      contentType: 'application/json',
+      dataType:'jsonp',
+      responseType:'application/json',
+      data: {
+          'key': '31a83692b8cfde09a94e3b299af8c46c',
+          'apikey': '31a83692b8cfde09a94e3b299af8c46c',
+      },
+      xhrFields: {
+        withCredentials: false
+      },
+      headers: {
+        'Access-Control-Allow-Credentials' : true,
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods':'GET',
+        'Access-Control-Allow-Headers':'application/json',
+        'user-key': '31a83692b8cfde09a94e3b299af8c46c',
+        'X-Zomato-API-Key': '31a83692b8cfde09a94e3b299af8c46c',
+      },
+      success: function(data) {
+        console.log(data);
+          console.log(data.locationSuggestions[0].id);
+          getRestaurantsByCityID(data.locationSuggestions[0].id, query);
+      },
+      error: function(error) {
+          $('#js-error-message').text(`Something went wrong: ${error.message}`);
+      }
+    });
+}
+
+
+function getRestaurantsByCityID(cityID, query) {
+    const searchURL = `https://developers.zomato.com/api/v2.1/search?q=${query}&entity_id=${cityID}&entity_type=city`;
     $.ajax({
         type: 'GET',
         url: searchURL,
@@ -32,35 +68,10 @@ function getLatLongByCityName(cityName, query) {
             $('#js-error-message').text(`Something went wrong: ${error.message}`);
         }
       });
-    // fetch(searchURL, {
-    //     // mode: 'cors',
-    //     'key': '31a83692b8cfde09a94e3b299af8c46c',
-    //     'apikey': '31a83692b8cfde09a94e3b299af8c46c',
-    //     headers: {
-    //         // 'Content-Type': 'application/json',
-    //         // 'user-key': '67646e13c44ac3bdc3cbb2e9f1081aa7',
-    //         // 'X-Zomato-API-Key': '67646e13c44ac3bdc3cbb2e9f1081aa7', 
-    //         'user-key': '31a83692b8cfde09a94e3b299af8c46c',
-    //         'X-Zomato-API-Key': '31a83692b8cfde09a94e3b299af8c46c',
-    //         // 'user-key': '460286dfbdb204719a6ef49dfdc82c58',
-    //         // 'X-Zomato-API-Key': '460286dfbdb204719a6ef49dfdc82c58'
-    //         // 'Access-Control-Allow-Origin':'*',
-    //     }
-    // })
-    //     .then(response => {
-    //         if (response.ok) {
-    //             return response.json();
-    //         }
-    //         throw new Error(response.statusText);
-    //     })
-    //     .then(responseJson => renderResult(responseJson, query))
-    //     .catch(err => {
-    //         $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    //     });
 }
 
 function priceRange(inputPriceRange) {
-    console.log(inputPriceRange);
+   
     let htmlOutPut = '';
     if (inputPriceRange == 1) {
 
@@ -92,77 +103,84 @@ function checkText(inputText) {
 
 //step 3: with the api results display them back to the user
 function renderResult(result, query) {
+  $('.js-search-results').html('');
     const outputElem = $('.js-search-results');
-    const rand = Math.floor(Math.random() * (result.restaurants.length - 1));
-    const rest = result.restaurants[rand];
-    console.log(rest, rand);
-    console.log(priceRange(rest.restaurant.priceRange));
-    const text = `
-	<div class="row top-results">
-	<div class="col-2">
-	<img src="${rest.restaurant.thumb}" class="thumb-img" alt="${checkText(rest.restaurant.name)}">
-	</div>
-	<div class="col-6">
-	<h2 class="restaurant-name">${checkText(rest.restaurant.name)}</h2><br>
-	<h3 class="city-name">${checkText(rest.restaurant.location.city)}</h3>
-	</div>
-	<div class="col-3">
-	<h3 class="user-rating">User Rating: <br>${checkText(rest.restaurant.userRating.rating_text)}</h3>
-	</div>
-	</div>
-	<div class="row">
-	<div class="col-6">
-	<p>Address: ${checkText(rest.restaurant.location.address)}</p>
-	<p class="price-range">${priceRange(rest.restaurant.priceRange)}</p>
-	<p>Average Cost for Two: $${checkText(rest.restaurant.average_cost_for_two)}</p>
-	<p>Cuisine: ${checkText(rest.restaurant.cuisines)}</p>
-	</div>
-	<div class="col-6">
-	<form action="https://www.google.com/maps/place/${checkText(rest.restaurant.location.latitude)},${checkText(rest.restaurant.location.longitude)}" target="_blank">
-	</form>
-	<form action="${rest.restaurant.menu_url}" target="_blank" >
-	<button type="submit" class="result-btn menu">Menu <i class="fas fa-utensils"></i></button>
-	</form>
-	</div>
-	</div>`;
+    let text = '';
+    let text_noimage = '';
+    for (let i = 0; i < result.restaurants.length; i++) {
+      let rest = result.restaurants[i];
+    
 
-    const text_noimage = `
-	<div class="row">
-	<div class="col-2">
-	<img src="rest.jpg" class="thumb-img" alt="restaurant">
-	</div>
-	<div class="col-6">
-	<h2 class="restaurant-name">${checkText(rest.restaurant.name)}</h2><br>
-	<h3 class="city-name">${checkText(rest.restaurant.location.city)}</h3>
-	</div>
-	<div class="col-3">
-	<h3 class="user-rating">User Rating: <br>${checkText(rest.restaurant.userRating.rating_text)}</h3>
-	</div>
-	</div>
-	<div class="row">
-	<div class="col-6">
-	<p>Address: ${checkText(rest.restaurant.location.address)}</p>
-	<p style="price-range">${priceRange(rest.restaurant.priceRange)}</p>
-	<p>Average Cost for Two: $${checkText(rest.restaurant.average_cost_for_two)}</p>
-	<p>Cuisine: ${checkText(rest.restaurant.cuisines)}</p>
-	</div>
-	<div class="col-6">
-	<form action="https://www.google.com/maps/place/${checkText(rest.restaurant.location.latitude)},${checkText(rest.restaurant.location.longitude)}" target="_blank">
-	<button type="submit" class="result-btn">Directions <i class="fas fa-map-marked-alt"></i> </button><br>
-	</form>
-	<form action="${rest.restaurant.menu_url}" target="_blank" >
-	<button type="submit" class="result-btn">Menu <i class="fas fa-utensils"></i></button>
-	</form>
-	</div>
-	</div>
-	`;
+     text = `
+      <div class="row top-results">
+      <div class="col-2">
+      <img src="${rest.restaurant.thumb}" class="thumb-img" alt="${checkText(rest.restaurant.name)}">
+      </div>
+      <div class="col-6">
+      <h2 class="restaurant-name">${checkText(rest.restaurant.name)}</h2><br>
+      <h3 class="city-name">${checkText(rest.restaurant.location.city)}</h3>
+      </div>
+      <div class="col-3">
+      <h3 class="user-rating">User Rating: <br>${checkText(rest.restaurant.userRating.rating_text)}</h3>
+      </div>
+      </div>
+      <div class="row">
+      <div class="col-6">
+      <p class="address">Address: ${checkText(rest.restaurant.location.address)}</p>
+      <p class="price-range">${priceRange(rest.restaurant.priceRange)}</p>
+      <p>Average Cost for Two: $${checkText(rest.restaurant.average_cost_for_two)}</p>
+      <p>Cuisine: ${checkText(rest.restaurant.cuisines)}</p>
+      </div>
+      <div class="col-6">
+      <form action="https://www.google.com/maps/place/${checkText(rest.restaurant.location.latitude)},${checkText(rest.restaurant.location.longitude)}" target="_blank">
+      </form>
+      <form action="${rest.restaurant.menuUrl}" target="_blank" >
+      <button type="submit" class="result-btn menu">Menu <i class="fas fa-utensils"></i></button>
+      </form>
+      </div>
+      </div>`;
 
-    if (rest.restaurant.thumb == "") {
-        $('.js-search-results').html(text_noimage);
+    text_noimage = `
+      <div class="row">
+      <div class="col-2">
+      <img src="rest.jpg" class="thumb-img" alt="restaurant">
+      </div>
+      <div class="col-6">
+      <h2 class="restaurant-name">${checkText(rest.restaurant.name)}</h2><br>
+      <h3 class="city-name">${checkText(rest.restaurant.location.city)}</h3>
+      </div>
+      <div class="col-3">
+      <h3 class="user-rating">User Rating: <br>${checkText(rest.restaurant.userRating.rating_text)}</h3>
+      </div>
+      </div>
+      <div class="row">
+      <div class="col-6">
+      <p>Address: ${checkText(rest.restaurant.location.address)}</p>
+      <p class="price-range">${priceRange(rest.restaurant.priceRange)}</p>
+      <p>Average Cost for Two: $${checkText(rest.restaurant.average_cost_for_two)}</p>
+      <p>Cuisine: ${checkText(rest.restaurant.cuisines)}</p>
+      </div>
+      <div class="col-6">
+      <form action="https://www.google.com/maps/place/${checkText(rest.restaurant.location.latitude)},${checkText(rest.restaurant.location.longitude)}" target="_blank">
+      <button type="submit" class="result-btn">Directions <i class="fas fa-map-marked-alt"></i> </button><br>
+      </form>
+      <form action="${rest.restaurant.menuUrl}" target="_blank" >
+      <button type="submit" class="result-btn">Menu <i class="fas fa-utensils"></i></button>
+      </form>
+      </div>
+      </div>
+      `;
+
+
+      if (rest.restaurant.thumb == "") {
+        $('.js-search-results').append(text_noimage);
     }
     else {
-        $('.js-search-results').html(text);
+        $('.js-search-results').append(text);
     }
+  }
+
+    
     $('.js-search-results').removeClass('hidden');
     $('.search-box').addClass('hidden');
     $('.box').addClass('hidden');
@@ -178,7 +196,7 @@ function watchSubmit() {
         // clear out the input
         // queryTarget.val("");
         console.log(cityName, query);
-        getLatLongByCityName(cityName, query);
+        getCityIdByCityName(cityName, query);
     });
 }
 
